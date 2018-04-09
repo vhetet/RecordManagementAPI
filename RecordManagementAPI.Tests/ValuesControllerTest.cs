07ee5874cf0232564a0b291ea0572f30f7caa084
Vincent <vincent.hetet@gmail.com>
2018-04-08 19:13:18 -0500

    updating gitignoore to remove obj files and adding tests project

---
+++ RecordManagementAPI.Tests/ValuesControllerTest.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RecordManagementAPI.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
