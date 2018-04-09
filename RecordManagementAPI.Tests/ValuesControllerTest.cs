6fd350791b3fbf400eeec7cd41ce7f0660d47793
Hetet, Vincent <vincent.hetet@united.com>
2018-04-09 16:58:36 -0500

    adding first test and updating gitignore

--- RecordManagementAPI.Tests/ValuesControllerTest.cs
+++ RecordManagementAPI.Tests/ValuesControllerTest.cs
@@ -8,6 +8,9 @@ namespace RecordManagementAPI.Tests
         [TestMethod]
         public void TestMethod1()
         {
+            var controller = new Controllers.ValuesController();
+            var result = controller.GetByEMail("test");
+            Assert.AreEqual("", result);
         }
     }
 }
